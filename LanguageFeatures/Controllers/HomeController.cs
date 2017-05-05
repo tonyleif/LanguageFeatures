8d23728b2d92ce181f596d72082791bbcd51d39c
Anthony Leif <tonyleif@hotmail.com>
2017-05-04 22:07:49 -0500

    Using Object and Collection Initializers

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-//using System.Collections.Generic;
+using System.Collections.Generic;
 //using System.Linq;
 //using System.Web;
 using System.Web.Mvc;
@@ -13,6 +13,50 @@ namespace LanguageFeatures.Controllers
         public string Index()
         {
             return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            //Create a new Product object
+            Product myProduct = new Product();
+
+            //Set the value property
+            myProduct.Name = "Kayak";
+
+            //Get the property
+            string productName = myProduct.Name;
+
+            //Generate the view
+            return View("Result", (object)String.Format("Product name: {0}", productName));
+        }
+
+        public ViewResult CreateProduct()
+        {
+            //Create and populate a new product object
+            Product myProduct = new Product
+            {
+                ProductID = 100,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
+
+            return View("Result", (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+
+            Dictionary<string, int> myDict = new Dictionary<string, int>
+            {
+                {"apple", 10 }, {"orange", 20}, {"plum", 30}
+            };
+
+            return View("Result", (object)stringArray[1]);
         }
     }
 }
