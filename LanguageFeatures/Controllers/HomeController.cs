e9d49e51b88380645dcec51ed6c84a5b2b16f649
Anthony Leif <tonyleif@hotmail.com>
2017-05-04 22:53:06 -0500

    Applying Extension Methods to an Interface

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -58,5 +58,55 @@ namespace LanguageFeatures.Controllers
 
             return View("Result", (object)stringArray[1]);
         }
+
+        public ViewResult UseExtension()
+        {
+            //Create and populate a shopping cart
+            ShoppingCart cart = new ShoppingCart()
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name="Kayak", Price = 275M},
+                    new Product {Name="Life Jacket", Price = 48.95M},
+                    new Product {Name="Soccer Ball", Price = 19.50M},
+                    new Product {Name="Corner Flag", Price = 34.95M}
+                }
+            };
+
+            //Get the total value of the products in the cart
+            decimal cartTotal = cart.TotalPrices();
+
+            return View("Result", (object)String.Format("Total: {0:c}", cartTotal));
+        }
+
+        public ViewResult UseExtensionEnumerable()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "Lifejacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.95M}
+                }
+            };
+
+            //Create and populate an array of product objects
+            Product[] productArray =
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+
+
+            //Get total values of the products in the cart
+            decimal cartTotal = products.TotalPrices();
+            decimal arrayTotal = productArray.TotalPrices();
+
+            return View("Result", (object)String.Format("Cart Total: {0}, Array Total: {1}", cartTotal, arrayTotal));
+        }
     }
 }
---
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> productEnum)
+        {
+            decimal total = 0;
+            foreach (Product prod in productEnum)
+            {
+                total += prod.Price;
+            }
+            return total;
+        }
+    }
+}
