e7a7bf1958ab336de9a474a35214e141e1726416
Anthony Leif <tonyleif@hotmail.com>
2017-05-07 17:41:57 -0500

    Using Lambda Expressions

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -108,5 +108,40 @@ namespace LanguageFeatures.Controllers
 
             return View("Result", (object)String.Format("Cart Total: {0}, Array Total: {1}", cartTotal, arrayTotal));
         }
+
+        public ViewResult UseFilterExtensionMethod()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Category="Watersports", Price = 275M},
+                    new Product {Name = "Lifejacket", Category="Watersports",  Price = 48.95M},
+                    new Product {Name = "Soccer ball", Category="Soccer", Price = 19.50M},
+                    new Product {Name = "Corner flag", Category="Soccer", Price = 34.95M}
+                }
+            };
+
+            //Func<Product, bool> categoryFilter = delegate (Product prod)
+            //{
+            //    return prod.Category == "Soccer";
+            //};
+
+            //Func<Product, bool> categoryFilter = prod => prod.Category == "Soccer";
+
+            //decimal total = 0;
+            //foreach (Product prod in products.Filter(categoryFilter))
+            //{
+            //    total += prod.Price;
+            //}
+
+            decimal total = 0;
+            foreach (Product prod in products.Filter(prod => prod.Category == "Soccer" || prod.Price > 20))
+            {
+                total += prod.Price;
+            }
+
+            return View("Result", (object)String.Format("Total: {0}", total));
+        }
     }
 }
